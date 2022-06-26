63aacab802bd2818366389c745678b2d523326cf
David Zwerdling <zwerdlds@gmail.com>
2022-06-26 12:30:29 -0700

    stubbed sha256

--- pkgs/games/pysolfc/default.nix
+++ pkgs/games/pysolfc/default.nix
@@ -7,16 +7,18 @@ buildPythonApplication rec {
 
   src = fetchzip {
     url = "https://versaweb.dl.sourceforge.net/project/pysolfc/PySolFC/PySolFC-${version}/PySolFC-${version}.tar.xz";
+    sha256 = "0fc819d28e2b14b84ff4f86b6983557d3e2b4353e19815724e4daeded3463774";
   };
 
   cardsets = fetchzip {
     url = "https://versaweb.dl.sourceforge.net/project/pysolfc/PySolFC-Cardsets/PySolFC-Cardsets-2.1/PySolFC-Cardsets-2.1.tar.bz2";
+    sha256 = "0fc819d28e2b14b84ff4f86b6983557d3e2b4353e19815724e4daeded3463774";
   };
 
   propagatedBuildInputs = with python3Packages; [
     tkinter six random2 configobj 
     # optional :
-    pygame freecell-solver pillow 
+    pygame freecell-solver pillow
   ];
 
   patches = [
